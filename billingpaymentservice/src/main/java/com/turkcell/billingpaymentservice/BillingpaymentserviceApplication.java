31bde8106135deee8f99ced2dfc89dd53b99e8f6
bayraktarmustafa <m.bayraktar01@hotmail.com>
2025-03-01 15:38:44 +0300

    commit

---
+++ billingpaymentservice/src/main/java/com/turkcell/billingpaymentservice/BillingpaymentserviceApplication.java
@@ -0,0 +1,13 @@
+package com.turkcell.billingpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingpaymentserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingpaymentserviceApplication.class, args);
+	}
+
+}
