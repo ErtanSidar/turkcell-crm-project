8c4a62594c9ed8916b7b04d6313cf16ea5aebc4e
EsmaDrcn <esmadurucan@gmail.com>
2025-03-02 21:25:19 +0300

    plansevice init

---
+++ planservice/src/test/java/com/turkcell/planservice/PlanserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.planservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
