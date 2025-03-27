21971da264f71973af8591ea77671fbc3ee253fd
ErtanSidar <essoft3@gmail.com>
2025-03-27 21:02:07 +0300

    add security config

---
+++ customerservice/src/main/java/com/turkcell/customerservice/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.turkcell.customerservice.config;
+
+import io.github.ertansidar.security.configuration.BaseSecurityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final BaseSecurityService baseSecurityService;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http = baseSecurityService.configureCoreSecurity(http);
+
+        // Özel security yapılandırması
+        // http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/customers/**").authenticated());
+
+        return http.build();
+    }
+}
