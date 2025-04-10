d0cb3769b93cfdd89175d4a9d777307edd033bf4
ErtanSidar <essoft3@gmail.com>
2025-04-11 00:42:46 +0300

    remove config

--- customerservice/src/main/java/com/turkcell/customerservice/config/SecurityConfig.java
+++ customerservice/src/main/java/com/turkcell/customerservice/config/SecurityConfig.java
@@ -12,14 +12,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final BaseSecurityService baseSecurityService;
-    private static final String[] WHITE_LIST_URLS = {
-            "/actuator/**"
-    };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http = baseSecurityService.configureCoreSecurity(http);
-        http.authorizeHttpRequests(req -> req.requestMatchers(WHITE_LIST_URLS).permitAll());
         http.authorizeHttpRequests(district -> district.requestMatchers("/api/v1/districts/**").permitAll());
         http.authorizeHttpRequests(city -> city.requestMatchers("/api/v1/cities/**").permitAll());
         http.authorizeHttpRequests(country -> country.requestMatchers("/api/v1/countries/**").permitAll());
