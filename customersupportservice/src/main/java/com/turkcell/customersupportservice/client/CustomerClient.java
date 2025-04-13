bd4bae70915499ce8fa525b6bab130d899505580
ErtanSidar <essoft3@gmail.com>
2025-04-13 20:46:40 +0300

    update custommer support feign

--- customersupportservice/src/main/java/com/turkcell/customersupportservice/client/CustomerClient.java
+++ customersupportservice/src/main/java/com/turkcell/customersupportservice/client/CustomerClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.UUID;
 
-@FeignClient(name = "customerservice")
+@FeignClient(name = "customerservice", url = "http://customerservice:8083")
 public interface CustomerClient {
 
     @GetMapping("/api/v1/customers/{id}")
